755d0f990e7988d9a3cead7fefad8ad64062a940
williamrodrigues1997 <ra99514@uem.br>
2020-09-25 23:38:45 -0300

    Add line chart.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,38 +3,76 @@ package main;
 import game.Board;
 import game.Game;
 import genetic.Genetic;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
 	public static void main(String[] args) {
+
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		stage.setTitle("Sudoku - Genetic Algorithm");
+
+		final NumberAxis xAxis = new NumberAxis();
+		final NumberAxis yAxis = new NumberAxis();
+		xAxis.setLabel("Generation");
+		yAxis.setLabel("Fitness");
+
+		final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+
+		lineChart.setTitle("Generation x Fitness");
+
+		XYChart.Series bestFitnessLine = new XYChart.Series();
+		bestFitnessLine.setName("Best Fitness");
+		XYChart.Series averagrFitnessLine = new XYChart.Series();
+		averagrFitnessLine.setName("Average fitness");
 
 		Board board = Game.generateBoard();
 		System.out.println("INITIAL BOARD TO SOLVE:");
 		board.printBoard();
-		
+
 		Genetic genetic = new Genetic(board);
 		genetic.initializePopulation();
-		
-		while(genetic.generation < util.Arguments.NUMBER_GENERATIONS) {			
+
+		while (genetic.generation < util.Arguments.NUMBER_GENERATIONS) {
 			genetic.populationFitness();
 			genetic.populattionProbability();
-			
-			System.out.println("Generation: " + genetic.generation 
-					+ "\nBest fitness: " + genetic.bestSolution.fitness 
-					+ "\nFitness average: " + genetic.calculatePopulationFitnessAverage()
-					+ "\nConflicts: " + genetic.bestSolution.conflicts + "\n");
-			
-			if(genetic.bestSolution.conflicts == 0) {
+
+			System.out.println("Generation: " + genetic.generation + "\nBest fitness: " + genetic.bestSolution.fitness
+					+ "\nFitness average: " + genetic.calculatePopulationFitnessAverage() + "\nConflicts: "
+					+ genetic.bestSolution.conflicts + "\n");
+
+			bestFitnessLine.getData().add(new XYChart.Data(genetic.generation, genetic.bestSolution.fitness));
+			averagrFitnessLine.getData()
+					.add(new XYChart.Data(genetic.generation, genetic.calculatePopulationFitnessAverage()));
+
+			if (genetic.bestSolution.conflicts == 0) {
 				System.out.println("SOLVED!");
 				break;
 			}
 			genetic.rouletteSelection();
 			genetic.newGeneration();
-			
+
 		}
-		
+
 		System.out.println("BEST SOLUTION");
 		genetic.bestSolution.printBoard();
+
+		Scene scene = new Scene(lineChart, 800, 600);
+		lineChart.getData().addAll(bestFitnessLine, averagrFitnessLine);
+
+		stage.setScene(scene);
+		stage.show();
+
 	}
 
 }
--- src/util/Arguments.java
+++ src/util/Arguments.java
@@ -6,8 +6,8 @@ public class Arguments {
 	public static double LIMIT_CONFLICTS_FITNESS = 100.0; //Max conflicts in a board = 216
 	public static int POPULATION_SIZE = 10000;	
 	public static int SELECTED_POPULATION_SIZE = 1000;
-	public static int ELITE_POPULATION = 900;
-	public static int NUMBER_GENERATIONS = 300;
+	public static int ELITE_POPULATION = 500;
+	public static int NUMBER_GENERATIONS = 100;
 	public static int MUTATION_RATE = 100; //Between 0 and 100
 	public static int MUTATION_SIZE = 1; //Number of cells altered in the mutation
 
